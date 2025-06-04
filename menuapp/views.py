700f43ab7c6381112b50abd755c103e9a089103c
lawrencelonghi <lawlonghi@gmail.com>
2025-06-04 13:06:59 -0300

    first commit

---
+++ menuapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import Produto
+from .models import Categoria
+
+
+def hello(request):
+    return HttpResponse('hello world')
+
+def menu(request):
+    # Get all categories with their related items
+    categorias = Categoria.objects.prefetch_related('produtos').all()
+    
+    return render(request, 'menu.html', categorias.data)
