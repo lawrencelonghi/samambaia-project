e031e42fa135a979dffab4ece880922bf8ccd8a7
lawrencelonghi <lawlonghi@gmail.com>
2025-08-05 21:29:57 -0300

    .env

--- core/urls.py
+++ core/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('menuapp.urls')),
-    path('menu/', include('menuapp.urls')),
+    path('', include('menuapp.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
