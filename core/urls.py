d892d88052dcc51d708ae8bc31a2b0745641cc94
lawrencelonghi <lawlonghi@gmail.com>
2025-08-07 10:11:19 -0300

    urls update

--- core/urls.py
+++ core/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('menuapp.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG or settings.ENVIRONMENT == 'production':
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
