dcfcbe2db16f77bb43bfe2b54342fabafcd099e5
lawrencelonghi <lawlonghi@gmail.com>
2026-01-12 18:35:00 -0300

    express server started

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+
+
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`)
+})
