4775a4782cc2e7490441bd700c4be0b56402676d
lawrencelonghi <lawlonghi@gmail.com>
2026-01-14 11:04:11 -0300

    categories routes created

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import path from "path";
 import authRoutes from './routes/auth.js'
+import categoriesRoutes from './routes/categories.js'
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/categories', categoriesRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`)
